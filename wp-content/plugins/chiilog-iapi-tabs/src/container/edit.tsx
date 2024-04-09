3fc1de147a9da3ece33d7ce3956d75fe42dd6d6d
chiilog <purgexpurge@gmail.com>
2024-04-09 15:47:51 +0900

    Add Tabでパネルも追加する

--- wp-content/plugins/chiilog-iapi-tabs/src/container/edit.tsx
+++ wp-content/plugins/chiilog-iapi-tabs/src/container/edit.tsx
@@ -1,6 +1,11 @@
+import { useDispatch } from '@wordpress/data';
 import { useState } from '@wordpress/element';
-import type { BlockEditProps } from '@wordpress/blocks';
-import { RichText, useBlockProps } from '@wordpress/block-editor';
+import { type BlockEditProps, createBlock } from '@wordpress/blocks';
+import {
+	RichText,
+	useBlockProps,
+	useInnerBlocksProps,
+} from '@wordpress/block-editor';
 import { Button } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { close } from '@wordpress/icons';
@@ -20,9 +25,24 @@ const defaultContents: TabItem = {
 export default function Edit( {
 	attributes: { contents },
 	setAttributes,
+	clientId,
 }: BlockEditProps< BlockAttributes > ) {
+	const { insertBlocks } = useDispatch( 'core/block-editor' );
+
 	// 選択中のタブが何番目かを保持するステート
 	const [ currentTab, setCurrentTab ] = useState( 0 );
+
+	const ALLOWED_BLOCKS = [ 'chiilog-iapi-tabs/panel' ];
+	const innerBlocksProps = useInnerBlocksProps(
+		{
+			className: 'smb-tabs__body',
+		},
+		{
+			allowedBlocks: ALLOWED_BLOCKS,
+			// @ts-ignore
+			renderAppender: false,
+		}
+	);
 
 	return (
 		<div { ...useBlockProps() }>
@@ -34,6 +54,19 @@ export default function Edit( {
 							contents: [ ...contents, defaultContents ],
 						} );
 						setCurrentTab( contents.length );
+						const createPanel = createBlock(
+							'chiilog-blocks/iapi-tabs-panel',
+							{},
+							[
+								createBlock( 'core/paragraph', {
+									content: __(
+										'Tab Content',
+										'chiilog-iapi-tabs'
+									),
+								} ),
+							]
+						);
+						insertBlocks( createPanel, contents.length, clientId );
 					} }
 				>
 					{ __( 'Add Tab', 'chiilog-iapi-tabs' ) }
@@ -47,9 +80,12 @@ export default function Edit( {
 					const tabId = index + 1;
 
 					return (
-						<div className="flex flex-col gap-1 flex-auto">
+						<div
+							key={ `nav-${ index }` }
+							className="flex flex-col gap-1 flex-auto"
+						>
 							{ currentTab === index ? (
-								<div>
+								<>
 									<Button
 										size="small"
 										icon={ close }
@@ -60,9 +96,10 @@ export default function Edit( {
 											setAttributes( {
 												contents: newContents,
 											} );
+											setCurrentTab( 0 );
 										} }
 									/>
-								</div>
+								</>
 							) : null }
 
 							<button
@@ -74,7 +111,6 @@ export default function Edit( {
 								id={ `tab-${ tabId }` }
 								aria-controls={ `panel-${ tabId }` }
 								tabIndex={ currentTab === index ? 0 : -1 }
-								key={ index }
 								onClick={ () => {
 									setCurrentTab( index );
 								} }
@@ -102,39 +138,7 @@ export default function Edit( {
 					);
 				} ) }
 			</div>
-			<div
-				id="panel-1"
-				role="tabpanel"
-				tabIndex={ 0 }
-				aria-labelledby="tab-1"
-				className="wp-block-chiilog-blocks-iapi-tabs__panel"
-				aria-expanded="true"
-				aria-hidden="false"
-			>
-				パネル1
-			</div>
-			<div
-				id="panel-2"
-				role="tabpanel"
-				tabIndex={ 0 }
-				aria-labelledby="tab-2"
-				className="wp-block-chiilog-blocks-iapi-tabs__panel"
-				aria-expanded="false"
-				aria-hidden="true"
-			>
-				パネル2
-			</div>
-			<div
-				id="panel-3"
-				role="tabpanel"
-				tabIndex={ 0 }
-				aria-labelledby="tab-3"
-				className="wp-block-chiilog-blocks-iapi-tabs__panel"
-				aria-expanded="false"
-				aria-hidden="true"
-			>
-				パネル3
-			</div>
+			<div { ...innerBlocksProps } />
 		</div>
 	);
 }
--- wp-content/plugins/chiilog-iapi-tabs/src/panel/edit.tsx
+++ wp-content/plugins/chiilog-iapi-tabs/src/panel/edit.tsx
@@ -2,7 +2,15 @@ import { useBlockProps, useInnerBlocksProps } from '@wordpress/block-editor';
 
 export default function Edit( {} ) {
 	return (
-		<div { ...useBlockProps() }>
+		<div
+			{ ...useBlockProps() }
+			id="panel-1"
+			role="tabpanel"
+			tabIndex={ 0 }
+			aria-labelledby="tab-1"
+			aria-expanded="true"
+			aria-hidden="false"
+		>
 			<div { ...useInnerBlocksProps() } />
 		</div>
 	);
