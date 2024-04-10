f97a7151dc29b1bb3a1dee88e88b070e2c20acb0
chiilog <purgexpurge@gmail.com>
2024-04-10 15:24:02 +0900

    renderを動的に書き換え

--- wp-content/plugins/chiilog-iapi-tabs/src/container/render.php
+++ wp-content/plugins/chiilog-iapi-tabs/src/container/render.php
@@ -1,82 +1,45 @@
 <?php
 /**
- * @param array    $attributes The block attributes.
- * @param string   $content    The block default content.
- * @param WP_Block $block      The block instance.
+ * @var array $attributes The block attributes.
+ * @var string $content The block content.
+ * @var WP_Block $block The block object.
  *
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
+
+$navItems = $attributes['contents'];
+
+wp_interactivity_state( 'myPlugin', array (
+	'totalTabs' => count( $navItems )
+));
 ?>
 <div
 	<?php echo get_block_wrapper_attributes(); ?>
 	data-wp-interactive="iapiTabs"
 >
 	<div class="wp-block-chiilog-blocks-iapi-tabs__nav" role="tablist">
-		<button
-			role="tab"
-			class="wp-block-chiilog-blocks-iapi-tabs__button"
-			data-wp-on--click="actions.selectTab"
-			aria-selected="true"
-			id="tab-1"
-			aria-controls="panel-1"
-			tabindex="0"
-		>
-			ナビ1
-		</button>
-		<button
-			role="tab"
-			class="wp-block-chiilog-blocks-iapi-tabs__button"
-			data-wp-on--click="actions.selectTab"
-			aria-selected="false"
-			id="tab-2"
-			aria-controls="panel-2"
-			tabindex="-1"
-		>
-			ナビ2
-		</button>
-		<button
-			role="tab"
-			class="wp-block-chiilog-blocks-iapi-tabs__button"
-			data-wp-on--click="actions.selectTab"
-			aria-selected="false"
-			id="tab-3"
-			aria-controls="panel-3"
-			tabindex="-1"
-		>
-			ナビ3
-		</button>
+		<?php
+		if ( $navItems ) :
+			foreach ( $navItems as $index => $navItem ) :
+				$tabNumber = $index + 1;
+				?>
+				<button
+					role="tab"
+					class="wp-block-chiilog-blocks-iapi-tabs__button"
+					data-wp-on--click="actions.selectTab"
+					aria-selected="<?php echo esc_attr( $index === 0 ? 'true' : 'false' ); ?>"
+					id="tab-<?php echo esc_attr( $tabNumber ); ?>"
+					aria-controls="panel-<?php echo esc_attr( $tabNumber ); ?>"
+					tabindex="<?php echo esc_attr( $index === 0 ? '0' : '-1' ); ?>"
+				>
+					<?php echo esc_html( $navItem['tabNavText'] ); ?>
+				</button>
+				<?php
+			endforeach;
+		endif;
+		?>
 	</div>
-	<div
-		id="panel-1"
-		role="tabpanel"
-		tabindex="0"
-		aria-labelledby="tab-1"
-		class="wp-block-chiilog-blocks-iapi-tabs__panel"
-		aria-expanded="true"
-		aria-hidden="false"
-	>
-		パネル1
-	</div>
-	<div
-		id="panel-2"
-		role="tabpanel"
-		tabindex="0"
-		aria-labelledby="tab-2"
-		class="wp-block-chiilog-blocks-iapi-tabs__panel"
-		aria-expanded="false"
-		aria-hidden="true"
-	>
-		パネル2
-	</div>
-	<div
-		id="panel-3"
-		role="tabpanel"
-		tabindex="0"
-		aria-labelledby="tab-3"
-		class="wp-block-chiilog-blocks-iapi-tabs__panel"
-		aria-expanded="false"
-		aria-hidden="true"
-	>
-		パネル3
+	<div class="wp-block-chiilog-blocks-iapi-tabs__panels">
+		<?php echo do_blocks( $content ); ?>
 	</div>
 </div>
